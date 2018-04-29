08993699a274fe1846ef80bf1b0cafe54ef2ea8d
Marvin Block <marvin.block1998@gmail.com>
2018-04-29 02:56:26 +0200

    .

--- Setup.bat
+++
@@ -1,6 +0,0 @@
-powershell -Command "Invoke-WebRequest https://nodejs.org/dist/v8.11.1/node-v8.11.1-x64.msi -OutFile node.msi"
-cd /d %~dp0
-msiexec.exe /i node.msi /qb
-call npm install discord.js
-call npm install fs
-call npm install node-watch
--- Start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-:start
-call node app.js
-goto start
